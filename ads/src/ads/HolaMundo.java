008d5045365210290d5e17a95d96a4dfa192c7c8
mmassetti <matiasmassetti@gmail.com>
2018-03-21 03:24:35 -0300

    Cambio archivo java

--- ads/src/ads/HolaMundo.java
+++ ads/src/ads/HolaMundo.java
@@ -3,7 +3,7 @@ package ads;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola massaurio.");
+		System.out.println("Hola massaurio Hola Igsub.");
 
 	}
 
